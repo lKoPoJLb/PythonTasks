019728c57d37b90fadc59d50d701ff6ccbb39c74
KoPoJLb <s.damir206@gmail.com>
2023-12-22 21:22:57 +0600

    main

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import os, json
-Data = open('python/main.json' 'r')
+Data = open('python/main.json', 'r', encoding='utf-8')
 txt = json.load(Data)
 
 print(txt['entry'])
 
 arr = input("Введите запрос: ").split()
-path = f'{os.getcwd()}/python'
+path = os.path.dirname(os.path.abspath(__file__))
 Fname, Tname, Tcount = arr[0], arr[1], int(arr[2])+1
 
 def CreatePath(Directory, File_Foler_Name):
-    if os.path.exists(Directory):
-        NewPath = CreatePath(path, Fname)
+    if not os.path.exists(Directory):
+        NewPath = os.path.join(path, Fname)
         os.mkdir(NewPath)
         print(txt['files']['foldercreated'])
-        return os.path.join(Directory, File_Foler_Name)
+    return os.path.join(Directory, File_Foler_Name)
 
 for i in range(1, Tcount):
     NewPath = CreatePath(f"{path}/{Fname}", f"{Tname}-{i}.py")
     file = open(NewPath, 'a')
     file.close()
 
-print(txt['files']['filercreated'])
+print(txt['files']['filecreated'])
 
 Data.close()
